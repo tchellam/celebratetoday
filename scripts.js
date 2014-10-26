e34a32548f1ce4e4dc4b6193c59ae6d79bf627f0
lizzie <lizzie@enchantedlabs.com>
2014-10-25 17:54:42 -0700

    yay pictures

---
+++ scripts.js
@@ -0,0 +1,60 @@
+function getHoliday() {
+  // find a holiday of today's date and populate html
+};
+
+var data = [
+  {
+    date: 2014-10-25,
+    name: 'Euskai Eguna',
+    desc: 'Also known as Day of the Basque Country, this holiday is celebrated in the Basque region of Spain.',
+    img: '/img/basque.jpg'
+  },
+  {
+    date: 2014-10-25,
+    name: 'World Pasta Day',
+    desc: 'Celebrate with a big plate of your favorite pasta! We suggest gnocchi with butternut squash. Yummm.',
+    img: '/img/pasta.jpg'
+  },
+  {
+    date: 2014-10-25,
+    name: 'Punk-for-a-Day Day',
+    desc: 'Ever considered a big colorful mohawk? Today\'s the day to try a radical new hairstyle.',
+    img: '/img/punk.jpg'
+  },
+  {
+    date: 2014-10-26,
+    name: 'Blah',
+    desc: 'Blah.',
+    img: '/img/blah.jpg'
+  },
+  {
+    date: 2014-10-26,
+    name: 'Blah',
+    desc: 'Blah.',
+    img: '/img/blah.jpg'
+  },
+  {
+    date: 2014-10-26,
+    name: 'Blah',
+    desc: 'Blah.',
+    img: '/img/blah.jpg'
+  },
+  {
+    date: 2014-10-27,
+    name: 'Blah',
+    desc: 'Blah.',
+    img: '/img/blah.jpg'
+  },
+  {
+    date: 2014-10-27,
+    name: 'Blah',
+    desc: 'Blah.',
+    img: '/img/blah.jpg'
+  },
+  {
+    date: 2014-10-27,
+    name: 'Blah',
+    desc: 'Blah.',
+    img: '/img/blah.jpg'
+  }
+];
